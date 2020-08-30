95e871fa7f4c63a637e2986f5a40fdc236eef60e
Debasish Sarker <debasishsarker36@gmail.com>
2020-08-30 17:08:50 +0600

    graph representation done

--- Graph/graph_representation/adjacency_list.py
+++ Graph/graph_representation/adjacency_list.py
@@ -1,2 +1,33 @@
+
 class Graph:
-    pass
+
+    def __init__(self, Nodes):
+        self.nodes = Nodes
+        self.adj_list = {}
+
+        for node in self.nodes:
+            self.adj_list[node] = []
+    
+    def add_edge(self, src, dest):
+        self.adj_list[src].append(dest)
+        self.adj_list[dest].append(src)
+    
+    def print_graph(self):
+        for node in self.nodes:
+            print(node, "-->", self.adj_list[node])
+
+
+nodes = ["A", "B", "C", "D", "E"]
+edges = [("A", "B"), ("A", "C"), ("A", "D"), ("B", "E"), ("D", "E")]
+
+graph = Graph(nodes)
+graph.print_graph()
+print()
+
+for u,v in edges:
+    graph.add_edge(u, v)
+
+graph.print_graph()
+
+
+    
